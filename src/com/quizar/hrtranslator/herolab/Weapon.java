dc01cae6c0e155451803c21c02d0ae23d302b46a
Mark <mark.g.romano@gmail.com>
2017-08-19 17:56:35 -0400

    Added parsing for crit, damage, and attack info for weapons.

--- src/com/quizar/hrtranslator/herolab/Weapon.java
+++ src/com/quizar/hrtranslator/herolab/Weapon.java
@@ -4,6 +4,9 @@ import javax.xml.bind.annotation.XmlAttribute;
 
 public class Weapon {
     private String name;
+    private String damage;
+    private String crit;
+    private String attack;
 
     public String getName() {
         return name;
@@ -12,5 +15,32 @@ public class Weapon {
     @XmlAttribute
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getDamage() {
+        return damage;
+    }
+
+    @XmlAttribute
+    public void setDamage(String damage) {
+        this.damage = damage;
+    }
+
+    public String getCrit() {
+        return crit;
+    }
+
+    @XmlAttribute
+    public void setCrit(String crit) {
+        this.crit = crit;
+    }
+
+    public String getAttack() {
+        return attack;
+    }
+
+    @XmlAttribute
+    public void setAttack(String attack) {
+        this.attack = attack;
     }
 }
