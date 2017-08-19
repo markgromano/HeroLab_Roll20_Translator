520a178319b0199bd04945c5b2e41cb61986b2ec
Mark <mark.g.romano@gmail.com>
2017-08-19 17:10:46 -0400

    Added file open dialog.

--- src/com/quizar/hrtranslator/frmMain.java
+++ src/com/quizar/hrtranslator/frmMain.java
@@ -1,10 +1,31 @@
 package com.quizar.hrtranslator;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class frmMain {
     private JList listCharacters;
     private JPanel mainPanel;
+    private JButton btnOpenXML;
+
+    public frmMain() {
+        btnOpenXML.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser chooser = new JFileChooser();
+                FileNameExtensionFilter filter = new FileNameExtensionFilter(
+                        "HeroLab Export File", "xml");
+                chooser.setFileFilter(filter);
+                int returnVal = chooser.showOpenDialog(mainPanel);
+                if(returnVal == JFileChooser.APPROVE_OPTION) {
+                    System.out.println("You chose to open this file: " +
+                            chooser.getSelectedFile().getAbsolutePath());
+                }
+            }
+        });
+    }
 
     public static void main(String args[]){
         JFrame frame = new JFrame("frmMain");
